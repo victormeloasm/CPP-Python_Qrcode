c46e0c141c3b07480c3a24e02f5b96d97f63f0f0
Víctor Duarte Melo <victormeloasm@gmail.com>
2024-09-08 08:55:28 -0300

    Update Gqrcode.py

--- Project/Gqrcode.py
+++ Project/Gqrcode.py
@@ -4,11 +4,11 @@ import qrcode
 from PIL import Image
 
 def generate_qr_code(data, output_file):
-    # Remove o arquivo se ele já existir
+
     if os.path.exists(output_file):
         os.remove(output_file)
     
-    # Cria uma instância do gerador de QR Code
+   
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -16,14 +16,10 @@ def generate_qr_code(data, output_file):
         border=4,
     )
     
-    # Adiciona os dados ao QR Code
+  
     qr.add_data(data)
     qr.make(fit=True)
-    
-    # Cria a imagem do QR Code
     img = qr.make_image(fill='black', back_color='white')
-    
-    # Salva a imagem no arquivo especificado
     img.save(output_file)
 
 if __name__ == "__main__":
@@ -33,6 +29,5 @@ if __name__ == "__main__":
     
     data = sys.argv[1]
     output_file = sys.argv[2]
-    
-    # Gera o QR Code
+
     generate_qr_code(data, output_file)
